12dc72b9f82e9a25bf6b28dce3d8e569cb5d615e
Jag <shettyjm@gmail.com>
2022-07-26 23:54:43 -0700

    back end urls as env

--- 03_OCP_Frontend/src/environments/environment.prod.ts
+++ 03_OCP_Frontend/src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
 
-  threescaleApiUrl : "https://edabackend-brighttalk.apps.rosa-l9gnh.cnq0.p1.openshiftapps.com",
-  // this.cityRootUrl = environment.threescaleApiCityUrl;
-  orderBackendUrl: "https://quarkus-kafka-quarkus-test.apps.rosa-l9gnh.cnq0.p1.openshiftapps.com"
-
+  SUPPORT_API_URL: "undefined",
+   ORDER_API_URL: "undefined"
 };
